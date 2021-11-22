a66bb15904778d0417a1185ab485424752c1bde5
keeth-office <keerthi.mac@gmail.com>
2021-11-22 13:14:07 +0530

    config original

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 // Option 1: Passing a connection URI
 module.exports = new Sequelize('codegig', 'root', '0542222175', {
   host: 'localhost',
-  dialect: 'mariadb' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+  dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 });
 
 
