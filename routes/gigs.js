2a03a3c37850d606e28c62b0d6a00a8121abb10f
keeth-office <keerthi.mac@gmail.com>
2021-11-22 16:29:36 +0530

    simple letters

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
-const Gig = require('../Models/Gig');
+const Gig = require('../models/gig');
 
 
 //get gig list
