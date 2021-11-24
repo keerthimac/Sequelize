46c8e2f27f072892f521af8d9a8c8276c88bbeb3
Darshana <keerthi.mac@gmail.com>
2021-11-24 07:48:44 +0530

    password null seq

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Passing a connection URI
-module.exports = new Sequelize('codegig', 'root', {
+module.exports = new Sequelize('codegig', 'root', null, {
   host: 'localhost',
   dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 });
